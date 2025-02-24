a135c81db16ceece83e8ff189fe22209e6838993
Denny Rubino <rubidenny@gmail.com>
2025-02-25 00:31:45 +0100

    add api

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const db = require("./db");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(require("cors")());
+
+app.get("/movies", async (req, res) => {
+  try {
+    const [movies] = await db.query("SELECT * FROM movies");
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/movies/:id", async (req, res) => {
+  const movieId = req.params.id;
+  try {
+    const [movie] = await db.query("SELECT * FROM movies WHERE id = ?", [
+      movieId,
+    ]);
+    const [reviews] = await db.query(
+      "SELECT * FROM reviews WHERE movie_id = ?",
+      [movieId]
+    );
+
+    if (movie.length === 0) {
+      return res.status(404).json({ message: "Film non trovato" });
+    }
+
+    res.json({ ...movie[0], reviews });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
